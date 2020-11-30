a73dbb5ef9686ae4f04e2f080bad4d42b9b7492b
Woozzang <woozzaang@gmail.com>
2020-11-30 15:39:50 +0900

    모의고사 - 개선1

--- programmers/level1/05.py
+++ programmers/level1/05.py
@@ -24,3 +24,29 @@ def solution(answers):
       answer.append(3)
 
     return answer
+
+# 개선 1
+
+def solution_1(answers):
+    pat1 = [1,2,3,4,5]
+    pat2 = [2,1,2,3,2,4,2,5]
+    pat3 = [3,3,1,1,2,2,4,4,5,5]
+    # 3개의 관련된 변수를 하나의 list에 넣음
+    score = [0, 0, 0]
+    result = []
+
+    for index, answer in enumerate(answers):
+      if answer == pat1[index % 5]:
+        score[0] += 1
+      if answer == pat2[index % 8]:
+        score[1] += 1
+      if answer == pat3[index % 10]:
+        score[2] += 1
+    
+    for index, s in enumerate(score):
+        # 단, 평가식마다 max()를 호출하게 되어 score 길이에 따라 오버헤드가 될 수도 있음
+        # max(score)은 고정된 값이므로 한번만 호출해서 변수에 저장해 놓는것이 나을 것이다.
+        if s == max(score):
+            result.append(index+1)
+
+    return result
